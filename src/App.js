0addf4a61e0551e17f019c55397f55f36ab5da7a
Andy-CSKim <andy.cskim@gmail.com>
2023-09-22 13:45:45 +0900

    update gif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+// import bulb_on from './pic_bulbon.gif'
+// import bulb_off from './pic_bulboff.gif'
+
+// function app() {}
 function App() {
+
+  const [btText, setBtText] = useState("on");
+
+  const myFunction = () => {
+    console.log("myFunction")
+    const img = document.getElementById("demo");
+    //const bt = document.getElementById("bt");
+    console.log(img.src)
+
+    if (img.src.includes("pic_bulboff.gif")) {
+        img.src = "pic_bulbon.gif";
+
+        //bt.innerHTML = "off"
+        setBtText("off")
+    } else {
+        img.src = "pic_bulboff.gif";
+        
+        //bt.innerHTML = "on"
+        setBtText("on")
+    }
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>This is a heading</h1>
+        <p>This is a paragraph.</p>
+
+        <button onClick={myFunction} >{btText}</button>
+        <br/>
+
+        <img id="demo" src="pic_bulboff.gif" />
+
       </header>
     </div>
   );
+
 }
 
 export default App;
